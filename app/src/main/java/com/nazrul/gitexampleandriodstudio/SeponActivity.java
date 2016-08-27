8d41932e41751a2c8b00fdcb17b50161974f9035
isepon <ismailhossainsepon@gmail.com>
2016-08-27 12:25:30 +0600

    Write somthing in textview

--- app/src/main/java/com/nazrul/gitexampleandriodstudio/SeponActivity.java
+++ app/src/main/java/com/nazrul/gitexampleandriodstudio/SeponActivity.java
@@ -2,8 +2,13 @@ package com.nazrul.gitexampleandriodstudio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SeponActivity extends AppCompatActivity {
+
+    private  EditText et1,et2, et3;
+    private Button submite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
